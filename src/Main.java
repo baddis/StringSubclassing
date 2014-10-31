e8b450f004fc8896f7f0de87878322fae3cd6e53
Bart Addis <bart.addis@mathworks.com>
2014-10-30 20:49:29 -0400

    first draft

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+
+
 public class Main {
 
+    static class Acr {
+        private String value;
+        public Acr(String value) {
+            this.value = value;
+        }
+        public String toString() {
+            return value;
+        }
+    }
+
+    static class Foo {
+        public String a(Acr val) {
+            return val.toString();
+        }
+        public Acr b( String val) {
+            return new Acr(val);
+        }
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Acr a = new Acr("asdflaksdjflaskdjf");
+        String s = new String("asdfasdflkjh");
+	    new Foo().a(a);
+        new Foo().b(s);
+        new Foo().b(a);
+    }
+
+    public void foo(String x) {
+        int i = x.length();
     }
 }
